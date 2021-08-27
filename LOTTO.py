a450ecfcaa6b9e4d20fcc02de31f3345da31504c
Nixir-hub <nixir@hotmail.com>
2021-08-28 00:31:02 +0200

    Program done.

--- LOTTO.py
+++ LOTTO.py
@@ -14,17 +14,17 @@ def LOTTO_game():
         lucky_numbers = []
         lucky_number = random.randint(1, 49)
         if lucky_number not in lucky_cupon:
-            lucky_numbers.append(lucky_number) #adds lucky number to list lucky_numbers
+            lucky_numbers.append(lucky_number)  #adds lucky number to list lucky_numbers
         elif lucky_number == 1:
-             lucky_numbers.append(lucky_number + 1)
+            lucky_numbers.append(lucky_number + 1)
         else:
-             lucky_numbers.append(lucky_number - 1)
+            lucky_numbers.append(lucky_number - 1)
         user_numbers = []
-        user_choice = input(f"Write your number {len(user_cupon) + 1}:" )
+        user_choice = input(f"Write your number {len(user_cupon) + 1}:")
         try:
-            if int(user_choice) == ValueError:   #if input is not int() raise ValueError
-               break
-            elif 1 > int(user_choice) or int(user_choice)> 49:
+            if int(user_choice) == ValueError:    #if input is not int() raise ValueError
+                break
+            elif 1 > int(user_choice) or int(user_choice) > 49:
                 print("Number must be in range 1 - 49!")
             elif int(user_choice) not in user_cupon:
                 user_numbers.append(int(user_choice))
@@ -33,9 +33,9 @@ def LOTTO_game():
             user_cupon += user_numbers
             lucky_cupon += lucky_numbers
         except ValueError:
-                print("Pick number!")
+            print("Pick number!")
         print(f"Your cupon: {sorted(user_cupon)}")
-    print(f"Win numbers: {sorted(lucky_cupon)}" )
-    return f'Your matching numbers: {set(sorted(user_cupon)) & set(sorted(lucky_cupon))}' #compare user_cupon with_lucky
+    print(f"Win numbers: {sorted(lucky_cupon)}")
+    return f'Your matching numbers: {set(sorted(user_cupon)) & set(sorted(lucky_cupon))}'  #compare usercupon with_lucky
 
 print(LOTTO_game())
