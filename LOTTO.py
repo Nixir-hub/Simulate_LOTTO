4208ff8c441a94434ba59b22a7518df26521f72f
Nixir-hub <nixir@hotmail.com>
2021-08-28 00:14:40 +0200

    Program done.

--- LOTTO.py
+++ LOTTO.py
@@ -1,21 +1,27 @@
 import random
 
-print("Lotto Game")
-print("Choose 6 numbers between from 1 to 49 ")
-
-
-
 def LOTTO_game():
+    """
+    Its console LOTTO game
+    :return: compare player cupon to lucky_cupon
+    """
+    print("Lotto Game")
+    print("Choose 6 numbers between from 1 to 49 ")
     user_cupon = []
     lucky_cupon = []
     while len(user_cupon) < 6:
         lucky_numbers = []
         lucky_number = random.randint(1, 49)
-        lucky_numbers.append(lucky_number)
+        if lucky_number not in lucky_cupon:
+            lucky_numbers.append(lucky_number) #adds lucky number to list lucky_numbers
+        elif lucky_number == 1:
+             lucky_numbers.append(lucky_number + 1)
+        else:
+             lucky_numbers.append(lucky_number - 1)
         user_numbers = []
         user_choice = input(f"Write your number {len(user_cupon) + 1}:" )
         try:
-            if int(user_choice) == ValueError:
+            if int(user_choice) == ValueError:   #if input is not int() raise ValueError
                break
             elif 1 > int(user_choice) or int(user_choice)> 49:
                 print("Number must be in range 1 - 49!")
@@ -27,7 +33,8 @@ def LOTTO_game():
             lucky_cupon += lucky_numbers
         except ValueError:
                 print("Pick number!")
-        print("Your cupon: ", sorted(user_cupon))
-    return f"Win numbers: {lucky_cupon}"
+        print(f"Your cupon: {sorted(user_cupon)}")
+    print(f"Win numbers: {sorted(lucky_cupon)}" )
+    return f'Your matching numbers {set(sorted(user_cupon)) & set(sorted(lucky_cupon))}' #compare user_cupon with_lucky
 
 print(LOTTO_game())
